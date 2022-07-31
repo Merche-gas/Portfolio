ec4b269275dc447e986dcdfc51adbfc1d5e02860
Merche-gas <merchegas75@gmail.com>
2022-07-31 19:24:07 +0200

    Avanzando en el portfolio

--- index.php
+++ index.php
@@ -13,6 +13,49 @@
         <section class="proyectos" id="seccionProyectos">
             <h1 class="proyectos__h1 animado">Sip, he trabajado muy duro. </h1>
             <p class="proyectos__p">En cada proyecto he puesto todo mi esfuerzo. </p>
+            <div class="proyectos__tarjeta uno">
+                <div class="proyectos__izda">
+                    <p>REDISEÑO WEB PARA PRÁCTICAS</p>
+                </div>
+                <div class="proyectos__centro">
+                    <div class="proyectos__overflow">
+                        <p>fantic <span>by merche</span> </p>
+                    </div>
+                </div>
+                <div class="proyectos__dcha">
+                    <h2 class="proyectos__h2">Fantic</h2>
+                    <a href="#" class="proyectos__a">VISITAR</a>
+                </div>
+            </div>
+            <div class="proyectos__tarjeta dos">
+                <div class="proyectos__izda">
+                    <p>REDISEÑO WEB PARA PRÁCTICAS</p>
+                </div>
+                <div class="proyectos__centro">
+                    <div class="proyectos__overflow">
+                        <p>fantic <span>by merche</span> </p>
+                    </div>
+                </div>
+                <div class="proyectos__dcha">
+                    <h2 class="proyectos__h2">Fantic</h2>
+                    <a href="#" class="proyectos__a">VISITAR</a>
+                </div>
+            </div>
+            <div class="proyectos__tarjeta tres">
+                <div class="proyectos__izda">
+                    <p>REDISEÑO WEB PARA PRÁCTICAS</p>
+                </div>
+                <div class="proyectos__centro">
+                    <div class="proyectos__overflow">
+                        <p>fantic <span>by merche</span> </p>
+                    </div>
+                </div>
+                <div class="proyectos__dcha">
+                    <h2 class="proyectos__h2">Fantic</h2>
+                    <a href="#" class="proyectos__a">VISITAR</a>
+                </div>
+            </div>
+            
 
         </section>
 
